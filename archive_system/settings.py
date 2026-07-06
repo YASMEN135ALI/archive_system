fd58dab531e8772fb97c3ca43b9d226b30d39955
YasmenAli <aliyasmen135@gmail.com>
2026-07-06 18:12:17 +0300

    Fix migrations, reset DB, update settings and urls

--- archive_system/settings.py
+++ archive_system/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'archive_system',
+    'archive_app',
     'rest_framework',
 ]
 
@@ -137,5 +137,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'archive_system.User'
+AUTH_USER_MODEL = 'archive_app.User'
+
 
